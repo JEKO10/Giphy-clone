c27a78e8fc802913a2334521ba8b130ed1981dd1
Aleksa Bubanja <aleksa3b@gmail.com>
2021-10-08 18:42:19 +0200

    created components folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
+import Loader from "./components/Loader";
+import Paginate from "./components/Paginate";
 
 function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  );
+  return <main></main>;
 }
 
 export default App;
---
+++ src/components/Paginate.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Paginate() {
+  return <div></div>;
+}
+
+export default Paginate;
