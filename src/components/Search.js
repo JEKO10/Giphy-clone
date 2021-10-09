040c37697e34aba27cddb81b03984a0e38569266
Aleksa Bubanja <aleksa3b@gmail.com>
2021-10-09 21:10:58 +0200

    search system is working

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const api = {
   base: "https://api.giphy.com/v1/gifs/",
@@ -13,17 +13,13 @@ function Search() {
   const fetchSearch = async () => {
     setIsLoading(true);
     const response = await fetch(
-      `${api.base}search?&api_key=${api.key}&q=ronaldo`
+      `${api.base}search?&api_key=${api.key}&q=${query}`
     );
     const data = await response.json();
     console.log(data);
     setSearchData(data.data);
     setIsLoading(false);
   };
-
-  useEffect(() => {
-    fetchSearch();
-  }, []);
 
   const renderSearch = () => {
     return searchData.map((item) => {
@@ -35,7 +31,32 @@ function Search() {
     });
   };
 
-  return <div className="container">{renderSearch()}</div>;
+  return (
+    <div className="container">
+      <section className="search">
+        <input
+          type="text"
+          placeholder="Search for gifs..."
+          onChange={(e) => {
+            setQuery(e.target.value);
+          }}
+          value={query}
+        />
+        <button
+          type="submit"
+          onClick={(e) => {
+            e.preventDefault();
+            fetchSearch();
+            setQuery("");
+            console.log(searchData);
+          }}
+        >
+          Search
+        </button>
+      </section>
+      <div className="container">{renderSearch()}</div>
+    </div>
+  );
 }
 
 export default Search;
