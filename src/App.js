769a8699b08cb4d005281d3cd4309543fb05083c
Aleksa Bubanja <aleksa3b@gmail.com>
2021-10-10 12:29:44 +0200

    paginate component return and styling finished

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import { useState } from "react/cjs/react.development";
 import Giphy from "./components/Giphy";
 import Search from "./components/Search";
-// import Paginate from "./components/Paginate";
+import Paginate from "./components/Paginate";
 
 function App() {
   const [searched, setSearched] = useState(false);
   return (
     <main>
       <Search func={setSearched} state={searched} />
+      <Paginate />
       {searched ? null : <Giphy />}
     </main>
   );
--- src/components/Paginate.js
+++ src/components/Paginate.js
@@ -3,13 +3,21 @@ import React from "react";
 function Paginate(props) {
   const pageNumber = [];
 
-  for (let i; i <= Math.ceil(props.total / props.itemsPer); i++) {
+  for (let i = 1; i <= Math.ceil(props.total / props.itemsPer); i++) {
     pageNumber.push(i);
   }
 
   return (
     <section className="container">
-      <ul className="pagination"></ul>
+      <ul className="pagination">
+        {pageNumber.map((item) => {
+          return (
+            <li>
+              <a href="!#">{item}</a>
+            </li>
+          );
+        })}
+      </ul>
     </section>
   );
 }
